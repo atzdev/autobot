cd2759f5c571090da1bb174f62f26d6222d69481
mookemjung <artzpluz@gmail.com>
2017-11-08 15:06:56 +0700

    image sending case

--- index.php
+++ index.php
@@ -21,25 +21,35 @@ $events = json_decode($content, true);
 if(!is_null($events['events'])) {
 	// Loop through each event
 	foreach($events['events'] as $event) {
+
+
+
 		// Line API send a lot of event type, we interested in message only
 		if($event['type'] == 'message') {
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
 			switch ($event['message']['type']) {
 				case 'text':
-					// Get replyToken
-					$replyToken = $event['replyToken'];
-
-					// Reply message
-					$respMessage = 'Hello, your message is :  '. $event['message']['text'];
-
 					$httpClient = new CurlHTTPClient($channel_token);
 					$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
 					$TextMessageBuilder = new TextMessageBuilder($respMessage);
 					$response = $bot->replyMessage($replyToken, $TextMessageBuilder);
 
 					break;
+				case 'image':
+					$messageId = $event['message']['id'];
+					$respMessage = 'Hello, your image ID is '. $messageID;
+					break;
+
+				default:
+					$respMessage = 'Please send image only';
+					break;
 				
 				
 			}
+
+			
 		}
 	}
 }
